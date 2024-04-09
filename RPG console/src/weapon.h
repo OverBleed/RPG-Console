adafd7c6ab7f37255708bc982ff4104c761e07e5
OverBleed <87242467+OverBleed@users.noreply.github.com>
2024-04-09 19:24:28 +0200

    Add files via upload

---
+++ RPG console/src/weapon.h
@@ -0,0 +1,25 @@
+#ifndef DEF_WEAPON
+#define DEF_WEAPON
+
+#include <string>
+
+class Weapon
+{
+	public:
+
+	Weapon();
+	Weapon(std::string name, std::string description, int damage);
+	std::string getWeaponName() const;
+	std::string getWeaponDescription() const;
+	int getWeaponDamage() const;
+	void changeWeapon(std::string name, std::string description, int damage);
+
+	private:
+
+	std::string m_name;
+	std::string m_description;
+	int m_damage;
+};
+
+
+#endif
